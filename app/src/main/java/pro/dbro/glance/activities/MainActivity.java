5bd0c2e0bd850216d4e77d570e421642f1709efb
FS <2019363+blackerking@users.noreply.github.com>
2025-04-04 13:41:25 +0200

    Update MainActivity.java

--- app/src/main/java/pro/dbro/glance/activities/MainActivity.java
+++ app/src/main/java/pro/dbro/glance/activities/MainActivity.java
@@ -9,7 +9,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
